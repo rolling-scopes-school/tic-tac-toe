5bc1c942eda64976ab07f6aabd38316e60d04cc1
Daria <qq.66@mail.ru>
2017-09-29 04:07:25 +0300

    Add files via upload

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,89 +1,221 @@
 class TicTacToe {
     constructor() {
-        this.CurrentSymbol="x";
-        this.board= [];
-        this.count_board=0;
-        this.winner=null;
+this.sign = 'x';
+    this.succ = undefined;
+    this.row0 = {
+        a: null,
+        b: null,
+        c: null
+ };
+        
+        this.row1 = {
+        a: null,
+        b: null,
+        c: null
+ };
+        
+        this.row2 = {
+        a: null,
+        b: null,
+        c: null
+ };
+    
+}
 
-        for(let i=0;i<3;i++) {
-            this.board[i]=[];
-            for(let y=0;y<3;y++){
-                this.board[i][y]=null;
-            }
-
+ getCurrentPlayerSymbol() {     
+return this.sign;
         }
-
-    }
-    getCurrentSymbol() {
-        return this.CurrentSymbol;
-    }
 
     nextTurn(rowIndex, columnIndex) {
-        if(rowIndex > 3 || columnIndex > 3 ){
-            return null;
-        } else {
+        if (rowIndex === 0) {
+            if (columnIndex == 0 ) {   
+                if (this.row0.a == null) {
+                 this.row0.a = this.sign;
+                 this.succ = 2;
+                
+                 }
+            } ;
 
-            if(this.board[rowIndex][columnIndex] === null){
-                this.board[rowIndex][columnIndex] = this.CurrentSymbol;
-                this.count_board++;
-            } else {
-                return null;
-            }
+                 if (columnIndex == 1) {
+                   if (this.row0.b == null) {
+                 this.row0.b = this.sign;
+                 this.succ = 2;
+                
+                 }  
+             };
+            
+          if (columnIndex == 2 ) {
+                if (this.row0.c == null) {
+                 this.row0.c = this.sign;
+                 this.succ = 2;
+                
+                 }
+            } ;     
+        };
+        
+     if (rowIndex == 1) {
+            if (columnIndex == 0 ) {
+                if (this.row1.a == null) {
+                 this.row1.a = this.sign;
+                 this.succ = 2;
+                 }
+            } ;
 
-            for( var i = 0; i < 3; i++){
-                if( (this.board[i][0] && this.board[i][0] === this.board[i][1] && this.board[i][1] === this.board[i][2]) ||
-                    (this.board[0][i] && this.board[0][i] === this.board[1][i] && this.board[1][i] === this.board[2][i]) ||
-                    (this.board[0][0] && this.board[0][0] === this.board[1][1] && this.board[1][1] === this.board[2][2]) ||
-                    (this.board[0][2] && this.board[0][2] === this.board[1][1] && this.board[1][1] === this.board[2][0]) )
-                    {
+         if (columnIndex == 1) {
+                   if (this.row1.b == null) {
+                 this.row1.b = this.sign;
+                 this.succ = 2;
+                 }  
+             };
+            
+          if (columnIndex == 2 ) {
+                
+                if (this.row1.c == null) {
+                 this.row1.c = this.sign;
+                 this.succ = 2;
+                 }
+            } ;     
+        };
+        
+        
+              if (rowIndex == 2) {
+           
+            if (columnIndex == 0 ) {
+                
+                if (this.row2.a == null) {
+                 this.row2.a = this.sign;
+                 this.succ = 2;
+                 }
+            } ;
 
-                        this.winner = this.CurrentSymbol;
-                    }
-                }
-
-            if(this.CurrentSymbol === 'x'){
-                this.CurrentSymbol = 'o';
-            } else {
-                this.CurrentSymbol = 'x';
-            }
-        }
-
+                 if (columnIndex == 1) {
+                   if (this.row2.b == null) {
+                 this.row2.b = this.sign;
+                 this.succ = 2;
+                 }  
+             };
+            
+          if (columnIndex == 2 ) {
+                
+                if (this.row2.c == null) {
+                 this.row2.c = this.sign;
+                 this.succ = 2;
+                 }
+            } ;     
+        };
+        
+ if (this.succ) {
+     if (this.sign === 'o' ) {
+    this.sign = 'x';
+this.succ = undefined;
+} else {
+    this.sign = 'o';
+    this.succ = undefined;
+}    
+ }       
+        return this.sign;
     }
 
     isFinished() {
-        if(this.winner||this.noMoreTurns()){
-        return true;
-    }
-
-    return false;
-
+        if ( this.row0.a == this.row0.b && this.row0.a == this.row0.c && this.row0.a !==null)
+              return true;
+        if ( this.row1.a == this.row1.b && this.row1.a == this.row1.c && this.row1.a !==null )
+              return true;
+        if (  this.row2.a == this.row2.b && this.row2.a == this.row2.c && this.row2.a !==null)
+              return true;
+        if ( this.row0.a == this.row1.a && this.row0.a == this.row2.a && this.row0.a !==null)
+              return true;
+        if ( this.row0.b == this.row1.b && this.row0.b == this.row2.b && this.row0.b !==null)
+              return true;
+        if ( this.row0.c == this.row1.c && this.row0.c == this.row2.c && this.row0.c !==null)
+              return true;
+        if ( this.row0.a == this.row1.b && this.row0.a == this.row2.c && this.row0.a !==null)
+              return true;
+        if ( this.row0.c == this.row1.b && this.row0.c == this.row2.a && this.row0.c !==null)
+              return true;
+        
+        if (this.row0.a !== null && this.row0.b !== null && this.row0.c !== null && this.row1.a !== null && this.row1.b !== null && this.row1.c !== null && this.row2.a !== null && this.row2.b !== null && this.row2.c !== null )
+            return true;
+        return false;
     }
 
     getWinner() {
-        return this.winner;
-
+        
+          if ( this.row0.a == this.row0.b && this.row0.a == this.row0.c && this.row0.a !==null)
+              return this.row0.a;
+        if ( this.row1.a == this.row1.b && this.row1.a == this.row1.c && this.row1.a !==null )
+              return this.row1.a;
+        if (  this.row2.a == this.row2.b && this.row2.a == this.row2.c && this.row2.a !==null)
+              return this.row2.a;
+        if ( this.row0.a == this.row1.a && this.row0.a == this.row2.a && this.row0.a !==null)
+              return this.row0.a;
+        if ( this.row0.b == this.row1.b && this.row0.b == this.row2.b && this.row0.b !==null)
+              return this.row0.b;
+        if ( this.row0.c == this.row1.c && this.row0.c == this.row2.c && this.row0.c !==null)
+              return this.row0.c;
+        if ( this.row0.a == this.row1.b && this.row0.a == this.row2.c && this.row0.a !==null)
+              return this.row0.a;
+        if ( this.row0.c == this.row1.b && this.row0.c == this.row2.a && this.row0.c !==null)
+              return this.row0.c;
+        return null;
     }
 
     noMoreTurns() {
-        if (this.count_board === 9){
-            return true;
-        }
-
-        return false;
+if (this.row0.a !== null && this.row0.b !== null && this.row0.c !== null && this.row1.a !== null && this.row1.b !== null && this.row1.c !== null &&this.row1.a !== null && this.row1.b !== null && this.row1.c !== null ) {
+    return true;
+} else {
+    return false;}
     }
 
     isDraw() {
-        if(this.noMoreTurns() && this.winner === null){
-            return true;
+        if ( this.row0.a == this.row0.b && this.row0.a == this.row0.c ||
+         this.row1.a == this.row1.b && this.row1.a == this.row1.c ||
+         this.row2.a == this.row2.b && this.row2.a == this.row2.c ||
+         this.row0.a == this.row1.a && this.row0.a == this.row2.a ||
+         this.row0.b == this.row1.b && this.row0.b == this.row2.b ||
+         this.row0.c == this.row1.c && this.row0.c == this.row2.c ||
+         this.row0.a == this.row1.b && this.row0.a == this.row2.c ||
+         this.row0.c == this.row1.b && this.row0.c == this.row2.a 
+       ) {
+            return false;
+        }
+        
+        if (this.row0.a == null || this.row0.b == null || this.row0.c == null || this.row1.a == null || this.row1.b == null || this.row1.c == null || this.row2.a == null || this.row2.b == null || this.row2.c == null )
+  {
+            return false;
+        }
+        return true;
     }
-    else {
-        return false;
-    }
-}
-
+    
     getFieldValue(rowIndex, colIndex) {
-        return this.board[rowIndex][colIndex];
+        
+if (rowIndex === 0) {
+            if (colIndex == 0 ) 
+                return this.row0.a;
+                 
+             if (colIndex == 1) 
+                { return this.row0.b} else 
+          return this.row0.c   
+        };
+ if (rowIndex === 1) {
+           
+            if (colIndex == 0 ) 
+                return this.row1.a;
+                 
+             if (colIndex == 1) 
+             {return this.row1.b} else 
+          return this.row1.c   
+        };       
+  if (rowIndex === 2) {
+           
+            if (colIndex == 0 ) 
+                return this.row2.a;
+                 
+             if (colIndex == 1) 
+             {return this.row2.b} else 
+          return this.row2.c   
+        };      
+        };    
     }
-}
 
 module.exports = TicTacToe;
